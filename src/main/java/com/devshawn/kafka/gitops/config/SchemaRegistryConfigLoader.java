5f8d4024db8aaa9cdf0ef0991b9e2ccfa263c2f9
Jerome Revillard <jerome.revillard@be-ys.com>
2021-08-10 15:36:57 +0200

    Tests

--- src/main/java/com/devshawn/kafka/gitops/config/SchemaRegistryConfigLoader.java
+++ src/main/java/com/devshawn/kafka/gitops/config/SchemaRegistryConfigLoader.java
@@ -1,7 +1,5 @@
 package com.devshawn.kafka.gitops.config;
 
-import com.devshawn.kafka.gitops.exception.MissingConfigurationException;
-import com.devshawn.kafka.gitops.exception.MissingMultipleConfigurationException;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
@@ -11,6 +9,12 @@ import java.util.concurrent.atomic.AtomicReference;
 public class SchemaRegistryConfigLoader {
 
     private static org.slf4j.Logger log = LoggerFactory.getLogger(SchemaRegistryConfigLoader.class);
+
+    public static final String SCHEMA_REGISTRY_URL_KEY = "SCHEMA_REGISTRY_URL";
+    public static final String SCHEMA_DIRECTORY_KEY = "SCHEMA_DIRECTORY";
+    public static final String SCHEMA_REGISTRY_SASL_JAAS_USERNAME_KEY = "SCHEMA_REGISTRY_SASL_JAAS_USERNAME";
+    public static final String SCHEMA_REGISTRY_SASL_JAAS_PASSWORD_KEY = "SCHEMA_REGISTRY_SASL_JAAS_PASSWORD";
+    public static final String SCHEMA_REGISTRY_SASL_CONFIG_KEY = "SCHEMA_REGISTRY_SASL_CONFIG";
 
     private SchemaRegistryConfigLoader() {}
 
@@ -28,14 +32,14 @@ public class SchemaRegistryConfigLoader {
         Map<String, String> environment = System.getenv();
 
         environment.forEach((key, value) -> {
-            if (key.equals("SCHEMA_REGISTRY_SASL_JAAS_USERNAME")) {
+            if (key.equals(SCHEMA_REGISTRY_SASL_JAAS_USERNAME_KEY)) {
                 username.set(value);
-            } else if (key.equals("SCHEMA_REGISTRY_SASL_JAAS_PASSWORD")) {
+            } else if (key.equals(SCHEMA_REGISTRY_SASL_JAAS_PASSWORD_KEY)) {
                 password.set(value);
-            } else if (key.equals("SCHEMA_REGISTRY_URL")) {
-                config.put("SCHEMA_REGISTRY_URL", value);
-            } else if (key.equals("SCHEMA_DIRECTORY")) {
-                config.put("SCHEMA_DIRECTORY", value);
+            } else if (key.equals(SCHEMA_REGISTRY_URL_KEY)) {
+                config.put(SCHEMA_REGISTRY_URL_KEY, value);
+            } else if (key.equals(SCHEMA_DIRECTORY_KEY)) {
+                config.put(SCHEMA_DIRECTORY_KEY, value);
             }
         });
 
@@ -50,13 +54,13 @@ public class SchemaRegistryConfigLoader {
     private static void handleDefaultConfig(Map<String, Object> config) {
         final String DEFAULT_URL = "http://localhost:8081";
         final String CURRENT_WORKING_DIR = System.getProperty("user.dir");
-        if (!config.containsKey("SCHEMA_REGISTRY_URL")) {
-            log.info("SCHEMA_REGISTRY_URL not set.  Using default value of {}", DEFAULT_URL);
-            config.put("SCHEMA_REGISTRY_URL", DEFAULT_URL);
+        if (!config.containsKey(SCHEMA_REGISTRY_URL_KEY)) {
+            log.info("{} not set.  Using default value of {}", SCHEMA_REGISTRY_URL_KEY, DEFAULT_URL);
+            config.put(SCHEMA_REGISTRY_URL_KEY, DEFAULT_URL);
         }
-        if (!config.containsKey("SCHEMA_DIRECTORY")) {
-            log.info("SCHEMA_DIRECTORY not set.  Defaulting to current working directory: {}", CURRENT_WORKING_DIR);
-            config.put("SCHEMA_DIRECTORY", CURRENT_WORKING_DIR);
+        if (!config.containsKey(SCHEMA_DIRECTORY_KEY)) {
+            log.info("{} not set.  Defaulting to current working directory: {}", SCHEMA_DIRECTORY_KEY, CURRENT_WORKING_DIR);
+            config.put(SCHEMA_DIRECTORY_KEY, CURRENT_WORKING_DIR);
         }
     }
 
@@ -65,13 +69,12 @@ public class SchemaRegistryConfigLoader {
             String loginModule = "org.apache.kafka.common.security.plain.PlainLoginModule";
             String value = String.format("%s required username=\"%s\" password=\"%s\";",
                     loginModule, escape(username.get()), escape(password.get()));
-            config.put("SCHEMA_REGISTRY_SASL_CONFIG", value);
-        } else if (username.get() != null) {
-            throw new MissingConfigurationException("SCHEMA_REGISTRY_SASL_JAAS_USERNAME");
-        } else if (password.get() != null) {
-            throw new MissingConfigurationException("SCHEMA_REGISTRY_SASL_JAAS_PASSWORD");
-        } else if (username.get() == null && password.get() == null) {
-            throw new MissingMultipleConfigurationException("SCHEMA_REGISTRY_SASL_JAAS_PASSWORD", "SCHEMA_REGISTRY_SASL_JAAS_USERNAME");
+            config.put(SCHEMA_REGISTRY_SASL_CONFIG_KEY, value);
+        } else {
+          if(config.get(SCHEMA_REGISTRY_SASL_CONFIG_KEY) == null) {
+              log.info("{} or {} not set. No authentication configured for the Schema Registry",
+                  SCHEMA_REGISTRY_SASL_JAAS_USERNAME_KEY, SCHEMA_REGISTRY_SASL_JAAS_PASSWORD_KEY);
+          }
         }
     }
 
--- src/main/java/com/devshawn/kafka/gitops/service/SchemaRegistryService.java
+++ src/main/java/com/devshawn/kafka/gitops/service/SchemaRegistryService.java
@@ -1,9 +1,6 @@
 package com.devshawn.kafka.gitops.service;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -12,15 +9,13 @@ import java.util.Map;
 import java.util.Optional;
 import org.apache.kafka.common.config.SaslConfigs;
 import com.devshawn.kafka.gitops.config.SchemaRegistryConfig;
+import com.devshawn.kafka.gitops.config.SchemaRegistryConfigLoader;
 import com.devshawn.kafka.gitops.domain.plan.SchemaPlan;
 import com.devshawn.kafka.gitops.domain.state.SchemaDetails;
+import com.devshawn.kafka.gitops.enums.SchemaCompatibility;
 import com.devshawn.kafka.gitops.enums.SchemaType;
 import com.devshawn.kafka.gitops.exception.SchemaRegistryExecutionException;
 import com.devshawn.kafka.gitops.exception.ValidationException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.flipkart.zjsonpatch.JsonDiff;
 import io.confluent.kafka.schemaregistry.AbstractSchemaProvider;
 import io.confluent.kafka.schemaregistry.ParsedSchema;
 import io.confluent.kafka.schemaregistry.SchemaProvider;
@@ -40,6 +35,10 @@ public class SchemaRegistryService {
 
     public SchemaRegistryService(SchemaRegistryConfig config) {
         this.config = config;
+    }
+
+    public Map<String, Object> getConfig() {
+      return Collections.unmodifiableMap(config.getConfig());
     }
 
     public List<String> getAllSubjects() {
@@ -66,69 +65,81 @@ public class SchemaRegistryService {
 
     public int register(SchemaPlan schemaPlan) {
         final CachedSchemaRegistryClient cachedSchemaRegistryClient = createSchemaRegistryClient();
-        ParsedSchema parsedSchema;
-        if(SchemaType.AVRO.toString().equalsIgnoreCase(schemaPlan.getSchemaDetails().get().getType())) {
-            AvroSchemaProvider avroSchemaProvider = new AvroSchemaProvider();
-            parsedSchema = avroSchemaProvider.parseSchema(
-                loadSchemaFromDisk(schemaPlan.getSchemaDetails().get().getFile()), Collections.emptyList()).get();
-        } else if (SchemaType.JSON.toString().equalsIgnoreCase(schemaPlan.getSchemaDetails().get().getType())) {
-            JsonSchemaProvider jsonSchemaProvider = new JsonSchemaProvider();
-            parsedSchema = jsonSchemaProvider.parseSchema(
-                loadSchemaFromDisk(schemaPlan.getSchemaDetails().get().getFile()), Collections.emptyList()).get();
-        } else if (SchemaType.PROTOBUF.toString().equalsIgnoreCase(schemaPlan.getSchemaDetails().get().getType())) {
-            ProtobufSchemaProvider protobufSchemaProvider = new ProtobufSchemaProvider();
-            parsedSchema = protobufSchemaProvider.parseSchema(loadSchemaFromDisk(
-                schemaPlan.getSchemaDetails().get().getFile()), Collections.emptyList()).get();
-        } else {
-            throw new ValidationException("Unknown schema type: " + schemaPlan.getSchemaDetails().get().getType());
+        AbstractSchemaProvider schemaProvider = schemaProviderFromType(schemaPlan.getSchemaDetails().get().getType());
+        ParsedSchema parsedSchema = parseSchema(schemaPlan.getName(), schemaPlan.getSchemaDetails().get(), schemaProvider, cachedSchemaRegistryClient);
+        int id;
+        try {
+            id = cachedSchemaRegistryClient.register(schemaPlan.getName(), parsedSchema);
+        } catch (IOException | RestClientException ex) {
+            throw new SchemaRegistryExecutionException("Error thrown when attempting to register subject '" + schemaPlan.getName() + "' in schema registry", ex.getMessage());
         }
         try {
-            return cachedSchemaRegistryClient.register(schemaPlan.getName(), parsedSchema);
+            cachedSchemaRegistryClient.updateCompatibility(schemaPlan.getName(), schemaPlan.getSchemaDetails().get().getCompatibility().get().toString());
         } catch (IOException | RestClientException ex) {
-            throw new SchemaRegistryExecutionException("Error thrown when attempting to register subject with schema registry", ex.getMessage());
+            throw new SchemaRegistryExecutionException("Error thrown when attempting to update compatibility of the newly registered subject '" + schemaPlan.getName() + "' in schema registry", ex.getMessage());
         }
+        return id;
     }
 
-    public void validateSchema(SchemaDetails schemaDetails) {
-        if (schemaDetails.getType().equalsIgnoreCase(SchemaType.AVRO.toString())) {
-            AvroSchemaProvider avroSchemaProvider = new AvroSchemaProvider();
-            validateSchema(schemaDetails, avroSchemaProvider);
-        } else if (schemaDetails.getType().equalsIgnoreCase(SchemaType.JSON.toString())) {
-            JsonSchemaProvider jsonSchemaProvider = new JsonSchemaProvider();
-            validateSchema(schemaDetails, jsonSchemaProvider);
-        } else if (schemaDetails.getType().equalsIgnoreCase(SchemaType.PROTOBUF.toString())) {
-            ProtobufSchemaProvider protobufSchemaProvider = new ProtobufSchemaProvider();
-            validateSchema(schemaDetails, protobufSchemaProvider);
+    public static AbstractSchemaProvider schemaProviderFromType(SchemaType schemaType) {
+        AbstractSchemaProvider schemaProvider;
+        if (schemaType == SchemaType.AVRO) {
+            schemaProvider = new AvroSchemaProvider();
+        } else if (schemaType == SchemaType.JSON) {
+            schemaProvider = new JsonSchemaProvider();
+        } else if (schemaType == SchemaType.PROTOBUF) {
+            schemaProvider = new ProtobufSchemaProvider();
         } else {
-            throw new ValidationException("Unknown schema type: " + schemaDetails.getType());
+            throw new ValidationException("Unknown schema type: " + schemaType);
+        }
+        return schemaProvider;
+    }
+
+    public void validateSchema(String subject, SchemaDetails schemaDetails) {
+        AbstractSchemaProvider schemaProvider = schemaProviderFromType(schemaDetails.getType());
+        validateSchema(subject, schemaDetails, schemaProvider);
+    }
+
+    public void validateSchema(String subject, SchemaDetails schemaDetails, AbstractSchemaProvider schemaProvider) {
+        CachedSchemaRegistryClient schemaRegistryClient = createSchemaRegistryClient();
+        ParsedSchema parsedSchema =  parseSchema(subject, schemaDetails, schemaProvider, schemaRegistryClient);
+        try {
+            List<String> differences = schemaRegistryClient.testCompatibilityVerbose(subject, parsedSchema);
+            if(! differences.isEmpty()) {
+                throw new ValidationException(String.format("%s schema '%s' is not compatible with the latest one: %s", schemaProvider.schemaType(), subject, differences));
+            }
+        } catch (IOException | RestClientException ex) {
+            throw new ValidationException(String.format("Error thrown when attempting to check the compatibility of the new schema for '%s': %s", subject, ex.getMessage()));
         }
     }
 
-    public void validateSchema(SchemaDetails schemaDetails, AbstractSchemaProvider schemaProvider) {
-      if (schemaDetails.getReferences().isEmpty()) {
-          Optional<ParsedSchema> parsedSchema = schemaProvider.parseSchema(loadSchemaFromDisk(schemaDetails.getFile()), Collections.emptyList());
-          if (!parsedSchema.isPresent()) {
-              throw new ValidationException(String.format("%s schema %s could not be parsed.", schemaProvider.schemaType(), schemaDetails.getFile()));
-          }
-      } else {
-          List<SchemaReference> schemaReferences = new ArrayList<>();
-          schemaDetails.getReferences().forEach(referenceDetails -> {
-              SchemaReference schemaReference = new SchemaReference(referenceDetails.getName(), referenceDetails.getSubject(), referenceDetails.getVersion());
-              schemaReferences.add(schemaReference);
-          });
-          // we need to pass a schema registry client as a config because the underlying code validates against the current state
-          schemaProvider.configure(Collections.singletonMap(SchemaProvider.SCHEMA_VERSION_FETCHER_CONFIG, createSchemaRegistryClient()));
-          try {
-              Optional<ParsedSchema> parsedSchema = schemaProvider.parseSchema(loadSchemaFromDisk(schemaDetails.getFile()), schemaReferences);
-              if (!parsedSchema.isPresent()) {
-                  throw new ValidationException(String.format("%s schema %s could not be parsed.", schemaProvider.schemaType(), schemaDetails.getFile()));
-              }
-          } catch (IllegalStateException ex) {
-              throw new ValidationException(String.format("Reference validation error: %s", ex.getMessage()));
-          } catch (RuntimeException ex) {
-              throw new ValidationException(String.format("Error thrown when attempting to validate %s schema with reference: %s", schemaProvider.schemaType(), ex.getMessage()));
-          }
-      }
+    private ParsedSchema parseSchema(String subject, SchemaDetails schemaDetails, AbstractSchemaProvider schemaProvider, CachedSchemaRegistryClient schemaRegistryClient) {
+        Optional<ParsedSchema> parsedSchema;
+        if (schemaDetails.getReferences().isEmpty()) {
+            parsedSchema = schemaProvider.parseSchema(schemaDetails.getSchema(), Collections.emptyList());
+            if (!parsedSchema.isPresent()) {
+                throw new ValidationException(String.format("%s schema for subject '%s' could not be parsed.", schemaProvider.schemaType(), subject));
+            }
+        } else {
+            List<SchemaReference> schemaReferences = new ArrayList<>();
+            schemaDetails.getReferences().forEach(referenceDetails -> {
+                SchemaReference schemaReference = new SchemaReference(referenceDetails.getName(), referenceDetails.getSubject(), referenceDetails.getVersion());
+                schemaReferences.add(schemaReference);
+            });
+            // we need to pass a schema registry client as a config because the underlying code validates against the current state
+            schemaProvider.configure(Collections.singletonMap(SchemaProvider.SCHEMA_VERSION_FETCHER_CONFIG, schemaRegistryClient));
+            try {
+                parsedSchema = schemaProvider.parseSchema(schemaDetails.getSchema(), schemaReferences);
+            } catch (IllegalStateException ex) {
+                throw new ValidationException(String.format("Reference validation error: %s", ex.getMessage()));
+            } catch (RuntimeException ex) {
+                throw new ValidationException(String.format("Error thrown when attempting to validate %s schema with reference: %s", subject, ex.getMessage()));
+            }
+            if (!parsedSchema.isPresent()) {
+                throw new ValidationException(String.format("%s referenced schema could not be parsed for subject %s", schemaProvider.schemaType(), subject));
+            }
+        }
+        return parsedSchema.get();
     }
 
     public SchemaMetadata getLatestSchemaMetadata(String subject) {
@@ -136,41 +147,49 @@ public class SchemaRegistryService {
         try {
             return cachedSchemaRegistryClient.getLatestSchemaMetadata(subject);
         } catch (IOException | RestClientException ex) {
-            throw new SchemaRegistryExecutionException("Error thrown when attempting to get delete subject from schema registry", ex.getMessage());
+            throw new SchemaRegistryExecutionException("Error thrown when attempting to get schema metadata for subject '" + subject + "'", ex.getMessage());
         }
     }
 
-    public String compareSchemasAndReturnDiff(String schemaStringOne, String schemaStringTwo) {
-        ObjectMapper objectMapper = new ObjectMapper();
+    public SchemaCompatibility getGlobalSchemaCompatibility() {
+        final CachedSchemaRegistryClient cachedSchemaRegistryClient = createSchemaRegistryClient();
         try {
-            JsonNode schemaOne = objectMapper.readTree(schemaStringOne);
-            JsonNode schemaTwo = objectMapper.readTree(schemaStringTwo);
-            JsonNode diff = JsonDiff.asJson(schemaOne, schemaTwo);
-            if (diff.isEmpty()) {
-                return null;
-            }
-            return diff.toString();
-        } catch (JsonProcessingException ex) {
-            throw new SchemaRegistryExecutionException("Error thrown when attempting to compare the schemas", ex.getMessage());
+            return SchemaCompatibility.valueOf(cachedSchemaRegistryClient.getCompatibility(null));
+        } catch (IOException | RestClientException ex) {
+            throw new SchemaRegistryExecutionException("Error thrown when attempting to get global schema compatibility", ex.getMessage());
         }
     }
-
-    public String loadSchemaFromDisk(String fileName) {
-        final String SCHEMA_DIRECTORY = config.getConfig().get("SCHEMA_DIRECTORY").toString();
+    public SchemaCompatibility getSchemaCompatibility(String subject, SchemaCompatibility globalCompatibility) {
+        final CachedSchemaRegistryClient cachedSchemaRegistryClient = createSchemaRegistryClient();
         try {
-            return new String(Files.readAllBytes(Paths.get(SCHEMA_DIRECTORY + "/" + fileName)), StandardCharsets.UTF_8);
+            return SchemaCompatibility.valueOf(cachedSchemaRegistryClient.getCompatibility(subject));
         } catch (IOException ex) {
-            throw new SchemaRegistryExecutionException("Error thrown when attempting to load a schema from schema directory", ex.getMessage());
+            throw new SchemaRegistryExecutionException("Error thrown when attempting to get schema compatibility for subject '" + subject + "'", ex.getMessage());
+        } catch (RestClientException ex) {
+            if(ex.getErrorCode() == 40401) {
+                return globalCompatibility;
+            }
+            throw new SchemaRegistryExecutionException("Error thrown when attempting to get schema compatibility for subject '" + subject + "'", ex.getMessage());
         }
+    }
+
+    public boolean deepEquals(SchemaDetails schemaDetails, SchemaMetadata schemaMetadata) {
+        AbstractSchemaProvider schemaProvider = schemaProviderFromType(schemaDetails.getType());
+        ParsedSchema newSchema = schemaProvider.parseSchema(schemaDetails.getSchema(), Collections.emptyList()).get();
+        ParsedSchema previousSchema = schemaProvider.parseSchema(schemaMetadata.getSchema(), Collections.emptyList()).get();
+        return (newSchema.deepEquals(previousSchema));
     }
 
     public CachedSchemaRegistryClient createSchemaRegistryClient() {
-        RestService restService = new RestService(config.getConfig().get("SCHEMA_REGISTRY_URL").toString());
-        SaslBasicAuthCredentialProvider saslBasicAuthCredentialProvider = new SaslBasicAuthCredentialProvider();
-        Map<String, Object> clientConfig = new HashMap<>();
-        clientConfig.put(SaslConfigs.SASL_JAAS_CONFIG, config.getConfig().get("SCHEMA_REGISTRY_SASL_CONFIG").toString());
-        saslBasicAuthCredentialProvider.configure(clientConfig);
-        restService.setBasicAuthCredentialProvider(saslBasicAuthCredentialProvider);
+        RestService restService = new RestService(config.getConfig().get(SchemaRegistryConfigLoader.SCHEMA_REGISTRY_URL_KEY).toString());
+        if(config.getConfig().get(SchemaRegistryConfigLoader.SCHEMA_REGISTRY_SASL_CONFIG_KEY) != null) {
+            SaslBasicAuthCredentialProvider saslBasicAuthCredentialProvider = new SaslBasicAuthCredentialProvider();
+            Map<String, Object> clientConfig = new HashMap<>();
+            clientConfig.put(SaslConfigs.SASL_JAAS_CONFIG, config.getConfig()
+                .get(SchemaRegistryConfigLoader.SCHEMA_REGISTRY_SASL_CONFIG_KEY).toString());
+            saslBasicAuthCredentialProvider.configure(clientConfig);
+            restService.setBasicAuthCredentialProvider(saslBasicAuthCredentialProvider);
+        }
         return new CachedSchemaRegistryClient(restService, 10);
     }
 
